90340a6c48fe4f2a0b37f09a69945da76fcc2429
Jonathan Slate <jslate@patientslikeme.com>
2016-01-12 12:57:21 -0500

    do not check in the HTML, remove adamant_test from list of svgs

--- bin/generate.rb
+++ bin/generate.rb
@@ -1,6 +1,6 @@
 require 'erubis'
 
-FILES = ['adamant_test.svg', "adamant.svg", "akward_she_head_thing.svg", "branch.svg", "branch_update_point.svg", "branch_want_keep.svg", "changes_on_multi_branches.svg", "deleted_rebased_from_branch.svg", "do_not_know_conflict.svg", "do_not_rely_on_ved.svg", "early_rebasing.svg", "generations.svg", "go_back_for_ancestor.svg", "individual.svg", "individuals.svg", "merge_with_ancestor.svg", "merging.svg", "more_rebasing.svg", "most_recent_common_ancestor.svg", "need_ancestor.svg", "no_new_timmoc.svg", "only_no_changes.svg", "this_is_a_timmocs.svg", "timmocs_are_not_people.svg", "triangle_horn.svg", "two_parents.svg", "ved.svg", "ved_intervene.svg",]
+FILES = ["adamant.svg", "akward_she_head_thing.svg", "branch.svg", "branch_update_point.svg", "branch_want_keep.svg", "changes_on_multi_branches.svg", "deleted_rebased_from_branch.svg", "do_not_know_conflict.svg", "do_not_rely_on_ved.svg", "early_rebasing.svg", "generations.svg", "go_back_for_ancestor.svg", "individual.svg", "individuals.svg", "merge_with_ancestor.svg", "merging.svg", "more_rebasing.svg", "most_recent_common_ancestor.svg", "need_ancestor.svg", "no_new_timmoc.svg", "only_no_changes.svg", "this_is_a_timmocs.svg", "timmocs_are_not_people.svg", "triangle_horn.svg", "two_parents.svg", "ved.svg", "ved_intervene.svg",]
 
 class Page
 
