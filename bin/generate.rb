f463f4f35d32a64bcd7b5d4b963d7ea21ca90953
Jonathan Slate <jslate@patientslikeme.com>
2016-01-13 10:56:31 -0500

    changes to generate, including handling keyboard events, next/previous

--- bin/generate.rb
+++ bin/generate.rb
@@ -1,23 +1,55 @@
 require 'erubis'
+require 'pry'
 
-FILES = ["adamant.svg", "akward_she_head_thing.svg", "branch.svg", "branch_update_point.svg", "branch_want_keep.svg", "changes_on_multi_branches.svg", "deleted_rebased_from_branch.svg", "do_not_know_conflict.svg", "do_not_rely_on_ved.svg", "early_rebasing.svg", "generations.svg", "go_back_for_ancestor.svg", "individual.svg", "individuals.svg", "merge_with_ancestor.svg", "merging.svg", "more_rebasing.svg", "most_recent_common_ancestor.svg", "need_ancestor.svg", "no_new_timmoc.svg", "only_no_changes.svg", "this_is_a_timmocs.svg", "timmocs_are_not_people.svg", "triangle_horn.svg", "two_parents.svg", "ved.svg", "ved_intervene.svg",]
+FILES = %w(
+  this_is_a_timmocs
+  timmocs_are_not_people
+  individuals
+  adamant
+  ved
+  generations
+  branch
+  branch_want_keep
+  branch_update_point
+  no_new_timmoc
+  only_no_changes
+  most_recent_common_ancestor
+  go_back_for_ancestor
+  two_parents
+  individual
+  merging
+  merge_with_ancestor
+  ved_intervene
+  need_ancestor
+  triangle_horn
+  do_not_rely_on_ved
+  akward_she_head_thing
+  changes_on_multi_branches
+  do_not_know_conflict
+  early_rebasing
+  more_rebasing
+  deleted_rebased_from_branch
+  )
 
 class Page
 
-  def initialize(previous, current, nxt)
-    @previous = previous
+  def initialize(current)
     @current = current
-    @nxt = nxt
+    index = FILES.index(@current)
+    @previous = FILES[index - 1] if index > 0
+    @nxt = FILES[index + 1] if index < FILES.count - 1
+    @text = File.read("text/#{@current}.txt").chomp
   end
+
   def html
     template = File.read('bin/template.erb')
     template = Erubis::Eruby.new(template)
-    template.result(current: @current)
+    template.result(current: @current, previous: @previous, nxt: @nxt, text: @text)
   end
 
 end
 
 FILES.each.with_index do |file, index|
-  html = Page.new(FILES[index - 1], file, FILES[index + 1]).html
-  File.open('html/' + file.gsub('.svg', '.html'), "w") { |io| io.puts html }
+  html = Page.new(file).html
+  File.open("html/#{file}.html", "w") { |io| io.puts html }
 end
